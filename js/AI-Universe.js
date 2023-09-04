841b8a369d22558c42148f0eebc573085731bba2
Fuad-hasan001 <spotlight.neutron0@gmail.com>
2023-09-04 23:39:19 +0600

    modal section updated

--- js/AI-Universe.js
+++ js/AI-Universe.js
@@ -46,6 +46,96 @@ const displayData = (techCards) => {
     toggleSpinner(false);
 }
 
+// -----------------Modal Data ---------------------------------
+const loadModalData = async (id) =>{
+    console.log(id);
 
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    displayModalData(data.data);
+}
+
+const displayModalData = (modalData) =>{
+    console.log(modalData);
+
+    const cardBodyContainer = document.getElementById('card-body-container');
+    cardBodyContainer.innerHTML = `
+            <h5 class="card-title fw-bold">${modalData.description}</h5>
+
+
+            <div class="row row-cols-1 row-cols-md-3 g-4 my-3">
+            <div class="col">
+                <div class="card border border-0">
+                    <div class="card-body mx-auto ">
+                        <h5 class = "fw-bold text-center text-success">${modalData.pricing[0].price}</h5>
+                        <h5 class = "fw-bold text-center text-success">${modalData.pricing[0].plan}</h5>
+                    </div> 
+                </div>       
+            </div>
+
+            <div class="col">
+                <div class="card border border-0">
+                    <div class="card-body">
+                    <h5 class = "fw-bold text-center text-warning">${modalData.pricing[1].price}</h5>
+                    <h5 class = "fw-bold text-center text-warning">${modalData.pricing[1].plan}</h5>
+                    </div> 
+                </div>       
+            </div>
+
+            <div class="col">
+                <div class="card border border-0">
+                    <div class="card-body text-center px-4">
+                    <h5 class = "fw-bold text-center text-danger">${modalData.pricing[2].price}</h5>
+                    <h5 class = "fw-bold text-center text-danger">${modalData.pricing[2].plan}</h5>
+                    </div>
+                </div>            
+            </div>
+        </div>
+
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card border border-0 bg-transparent">
+                        <div class="card-body">
+                            <h5 class = "fw-bold">Features</h5>
+                            <p>
+                                <li><small class="text-body-secondary">${modalData.features[1].feature_name}</small> <br></li>
+                                <li><small class="text-body-secondary">${modalData.features[2].feature_name}</small> <br></li>
+                                <li><small class="text-body-secondary">${modalData.features[3].feature_name}</small></li>
+                            </p>
+                        </div> 
+                    </div>       
+                </div>
+
+                <div class="col">
+                    <div class="card border border-0 bg-transparent">
+                        <div class="card-body">
+                                <h5 class="fw-bold">integrations</h5>
+                                <p>
+                                    <li><small class="text-body-secondary">${modalData.integrations[0]}</small> <br></li>
+                                    <li><small class="text-body-secondary">${modalData.integrations[1]}</small> <br></li>
+                                    <li><small class="text-body-secondary">${modalData.integrations[2]}</small></li>
+                                </p>
+                        </div>
+                    </div>            
+                </div>
+            </div>
+    `
+    const modalImageContainer = document.getElementById('modal-image');
+    modalImageContainer.innerHTML = `
+        <div class="position-relative">
+            <button id="btn-accuracy" type="button" class="btn btn-danger fs-5 position-absolute top-0 end-0 mt-2 me-2 badge border border-0 fs-5">
+            <span">${modalData.accuracy.score * 100 + '%'}</span> accuracy
+            </button>
+        </div>
+        <img  src="${modalData.image_link[0]}" class="card-img-top" alt="...">
+
+        <div class="card-body text-center">
+            <!-- input_output_examples -->
+            <h5 class="card-title fw-bold">${modalData.input_output_examples[0].input}</h5>
+            <p class="card-text">${modalData.input_output_examples[0].output}</p>
+        </div>
+    `
+}
 
 toggleSpinner(true)
